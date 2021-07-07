7fab7081b7d209728e7cb48ef02604b247ddab11
Lungu Silviu <39781744+YeLLoLS@users.noreply.github.com>
2021-07-07 23:25:23 +0000

    changed the activity status

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f"{client.user} logged in now!")
+    await client.change_presence(activity=discord.Game(name="barbut. Răspund doar celor care aruncă cu $112!"))
 
 
 @client.event
