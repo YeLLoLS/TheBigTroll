cdaef2e90adff6f769cb596ddc1e3f119baf1089
Lungu Silviu <39781744+YeLLoLS@users.noreply.github.com>
2021-07-07 23:02:39 +0000

    guess game added

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ async def on_message(message):
       embed = discord.Embed(title="Comenzi utile pentru a-ți Alina suferința!")
       embed.add_field(name="$alin_hainoroc", value="Lasă-l pe Alin să te salute!")
       embed.add_field(name="$alin_ganduri", value="Lasă-l pe Alin să agațe în locul tău sau invață de la EL!")
+      embed.add_field(name="$alin_ghiceste", value="Pierzi la barbut și devin-o sclavul lui Alin!")
       await message.channel.send(content=None, embed=embed)
 
     elif message.content == "$alin_hainoroc":
@@ -39,6 +40,24 @@ async def on_message(message):
 
     elif any(x in message.content for x in cuvinte_restrictionate):
       await message.delete()
+    
+    elif message.content == '$alin_ghiceste':
+            await message.channel.send('Te provoc să ghicești un număr între 1 și 10. Dacă nu ghicești ești sclavul meu.')
+
+            def is_correct(m):
+                return m.author == message.author and m.content.isdigit()
+
+            answer = random.randint(1, 10)
+
+            try:
+                guess = await client.wait_for('message', check=is_correct, timeout=5.0)
+            except asyncio.TimeoutError:
+                return await message.channel.send(f'La cât de încet ești, va trece viața pe lângă tine... Răspunsul corect era {answer}.')
+
+            if int(guess.content) == answer:
+                await message.channel.send('Mânca-ți-aș, ești mai ceva decât o ghicitoare!')
+            else:
+                await message.channel.send(f'Boss-ule, te aștept pe plantație. Răspunsul era {answer}, doar ca să mori de ciudă.')
 
 
 my_secret = os.environ['TOKEN']
